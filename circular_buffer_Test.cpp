1645df81e88e16b4731b1a41dbfaf8aab41c65c1
赵方程 <equationzhao@foxmail.com>
2021-12-27 01:56:20 +0800

    update: +,-,+=,-= for iterator member variable distance(the distance of the head to here in forward-order) ! Need TEST!

--- circular_buffer_Test.cpp
+++ circular_buffer_Test.cpp
@@ -1,5 +1,6 @@
 // #include <gtest/gtest.h>
 #include "circular_buffer.h"
+#include <algorithm>
 #include <chrono>
 #include <iostream>
 #include <list>
@@ -19,7 +20,7 @@ struct data
 {
 	std::vector<std::list<uint16_t>> a;
 	int c;
-	double *b;
+	double* b;
 	std::unique_ptr<std::map<std::string, int>> e;
 
 	data(int aa): c(aa)
@@ -32,7 +33,7 @@ struct data
 		}
 
 		b = new double(321);
-		e = std::make_unique<std::map<std::string, int>>(std::map<std::string, int> {{"12", 1}});
+		e = std::make_unique<std::map<std::string, int>>(std::map<std::string, int>{{"12", 1}});
 
 		++ctimes;
 	}
@@ -48,12 +49,12 @@ struct data
 
 		c = 1;
 		b = new double(321);
-		e = std::make_unique<std::map<std::string, int>>(std::map<std::string, int> {{"12", 1}});
+		e = std::make_unique<std::map<std::string, int>>(std::map<std::string, int>{{"12", 1}});
 
 		++ctimes;
 	}
 
-	data(const data &other) : c(other.c), b(new double(*other.b))
+	data(const data& other) : c(other.c), b(new double(*other.b))
 	{
 		std::cout << "copy\n";
 		auto temp = *other.e;
@@ -61,15 +62,19 @@ struct data
 		++ctimes;
 	}
 
-	data(data &&other) : a(std::move(other.a)), c(other.c), b(other.b), e(std::move(other.e))
+	data(data&& other) : a(std::move(other.a)), c(other.c), b(other.b), e(std::move(other.e))
 	{
 		std::cout << "move\n";
 		other.b = nullptr;
 		++ctimes;
 	}
 
-	data &operator =(const data &other) noexcept
+	data& operator =(const data& other) noexcept
 	{
+		if (this == std::addressof(other))
+		{
+			return *this;
+		}
 		std::cout << "copy\n";
 		a = other.a;
 		c = other.c;
@@ -79,8 +84,12 @@ struct data
 		return *this;
 	}
 
-	data &operator =(data &&other) noexcept
+	data& operator =(data&& other) noexcept
 	{
+		if (this == std::addressof(other))
+		{
+			return *this;
+		}
 		std::cout << "move\n";
 		a = std::move(other.a);
 		c = other.c;
@@ -105,110 +114,45 @@ int main()
 	const auto start = std::chrono::high_resolution_clock::now();
 
 	{
-		// std::list<data> list;
-		// for (int i = 0; i < N; ++i)
-		// {
-		// 	list.emplace_back(data());
-		// }
 		CircularBuffer<data> buffer(N), buffer2(2 * N);
 
-		// CircularBuffer<int> buffer2{10086};
-		// auto begin0 = buffer.begin();
-		// auto end0 = buffer.end();
-		// const auto begin1 = buffer.begin();
-		// const auto end1 = buffer.end();
-		// const auto& begin2 = buffer.begin();
-		// const auto& end2 = buffer.end();
-		// auto&& begin3 = buffer.begin();
-		// auto&& end3 = buffer.end();
-		// auto begin5 = buffer.cbegin();
-		// auto end5 = buffer.cend();
-		// const auto begin6 = buffer.cbegin();
-		// const auto end6 = buffer.cend();
-		// const auto& begin7 = buffer.cbegin();
-		// const auto& end7 = buffer.cend();
-		// auto&& begin8 = buffer.cbegin();
-		// auto&& end8 = buffer.cend();
-
-
-		// for (size_t i = 0, end = buffer.capacity(); i < end; ++i)
-		// {
-		// 	buffer.write(i);
-		// }
-		//
-		// for (size_t i = 0, end = buffer2.capacity(); i < end; ++i)
-		// {
-		// 	buffer2.write(i);
-		// }
-		//
-		//
-		// for (auto i : buffer)
-		// {
-		// 	std::cout << i.c << '\n';
-		// }
-		//
-		//
-		// buffer.swap(buffer);
-		//
-		// for (auto i : buffer)
-		// {
-		// 	std::cout << i.c << '\n';
-		// }
-
-
-		//
-		// for (const auto& i : buffer)
-		// {
-		// 	std::cout << i.c << '\n';
-		// }
-
-		// for (auto& i : buffer)
-		// {
-		// 	std::cout << i.c << '\n';
-		// }
-		//
-		// for (auto&& i : buffer)
-		// {
-		// 	std::cout << i.c << '\n';
-		// }
-		//
-		// for (auto i = buffer.begin(); i != buffer.end(); ++i)
-		// {
-		// 	std::cout << i->c << '\n';
-		// }
-		//
-		// for (auto&& i = buffer.begin(); i != buffer.end(); ++i)
-		// {
-		// 	std::cout << i->c << '\n';
-		// }
-
-		// for (size_t i = 0, end = buffer.capacity(); i < end; ++i)
-		// {
-		// 	std::cout << buffer.read().c << '\n';
-		// }
-
-		// for (size_t i = 0, end = buffer.capacity(); i < end; ++i)
-		// {
-		// 	std::cout << buffer[i].c << '\n';
-		// }
-
-		data c(100);
 
 		for (size_t i = 0, end = buffer.capacity(); i < end; ++i)
 		{
-			buffer.write(std::move(c));
+			buffer.write(100 - i);
 		}
 
-		//
-		// for (const auto& data : buffer)
-		// {
-		// 	std::cout << data.c << '\n';
-		// }
+		for (size_t i = 0, end = buffer2.capacity(); i < end; ++i)
+		{
+			buffer2.write(39 - i);
+		}
+
+
+		for (auto i : buffer)
+		{
+			std::cout << i.c << '\n';
+		}
+
+
+		buffer.swap(buffer);
+
+		for (auto i : buffer)
+		{
+			std::cout << i.c << '\n';
+		}
+
+		std::sort(buffer.begin(), buffer.end());
+
+
+		for (auto&& i : buffer)
+		{
+			std::cout << i.c << '\n';
+		}
 	}
 
 	const auto end = std::chrono::high_resolution_clock::now();
 
 	std::cout << "costing time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
-	          << "ms\n";
+		<< "ms\n";
 	std::cout << ctimes << "\n" << dtimes << "\n";
 }
